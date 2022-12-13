4deaf3075c0de0f3ad963bcb9220d82cf5ec5f35
Aslıhan <aslihan_hasar@hotmail.com>
2022-12-13 21:59:09 +0300

    Add program codes via upload

---
+++ src/PerfectNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number, i;
+        do {
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+            int sum = 0;
+            for (i = 1; i < number; i++) {
+                if (number % i == 0) {
+                    sum += i;
+                }
+            }
+            if (sum == number) {
+                System.out.println("This number is a perfect number.");
+
+            } else {
+                System.out.println("This number is not a perfect number.\n" +
+                        "Try a different number.");
+            }
+        } while (number > 0);
+        System.out.println("Invalid Input! Enter a positive number.");
+    }
+}
